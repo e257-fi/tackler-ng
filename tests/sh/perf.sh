a293ee3b3b90c18ec26ccaa29eab9da81a0b4734
35V LG84 <35vlg84-x4e6b92@e257.fi>
2025-01-11 15:48:40 +0200

    Replace full copyright header with SPDX
    Signed-off-by: 35V LG84 <35vlg84-x4e6b92@e257.fi>

--- tests/sh/perf.sh
+++ tests/sh/perf.sh
@@ -2,11 +2,3 @@
 #
 # This is not a real performance test, see 
 # $tackler-ng/perf -directory for the actual setup
-#
-# test: f6ee1b19-c16d-4219-a9cb-3252a38834ee
-# desc: 1E3 tests
-#
-# test: 66b32e8b-ce65-4dc1-9108-5ee5cd009cd3
-# desc: 1E6 tests
-
-exit 0
